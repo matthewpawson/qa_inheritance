5b46ec785b9ec669cf6b23ca2016feb93d7b8a9b
Matthew Pawson <matthewpawson@talktalk.net>
2022-10-25 14:20:45 +0100

    Added constructing chaining and some objects

--- qa_poly/Animal/Animal.java
+++ qa_poly/Animal/Animal.java
@@ -1,4 +1,13 @@
 public class Animal {
+
+    protected String name;
+
+    public Animal() {
+
+    }
+    public Animal(String name){
+        this.name = name;
+    }
     public void rest() {
         System.out.println("Animal rested.");
     }
@@ -16,21 +25,27 @@ public class Animal {
     }
     @Override
     public String toString() {
-        return "This is an animal.";
+        return "This is an animal, named " + name + ".";
     }
 }
 
 class Fish extends Animal {
+    public Fish(String name){
+        super(name);
+    }
     public void swim() {
         System.out.println("Fish swam.");
     }
     @Override
     public String toString() {
-        return "This is a fish, a type of animal.";
+        return "This is a fish, a type of animal. It is named " + name + ".";
     }
 }
 
 class Bird extends Animal {
+    public Bird(String name) {
+        super(name);
+    }
     public void fly() {
         System.out.println("Bird flew.");
     }
@@ -42,28 +57,34 @@ class Bird extends Animal {
     }
     @Override
     public String toString() {
-        return "This is a bird, a type of animal.";
+        return "This is a bird, a type of animal. It is named " + name + ".";
     }
 }
 
 class Penguin extends Bird {
+    public Penguin(String name) {
+        super(name);
+    }
     @Override
     public void fly() {
         System.out.println("Penguin cannot fly!");
     }
     @Override
     public String toString() {
-        return "This is a penguin, a type of bird.";
+        return "This is a penguin, a type of bird. It is named " + name + ".";
     }
 }
 
 class Owl extends Bird {
+    public Owl(String name){
+        super(name);
+    }
     @Override
     public void rest() {
         System.out.println("Owl rested during the day.");
     }
     @Override
     public String toString() {
-        return "This is an owl, a type of bird.";
+        return "This is an owl, a type of bird. It is named " + name + ".";
     }
 }
--- qa_poly/Animal/Main.java
+++ qa_poly/Animal/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Animal collard = new Bird("collard");
+        Bird spoonbill = new Bird("spoonbill");
+        Penguin penguin = new Penguin("penguin");
+        Animal[] animalArray = {penguin, collard, spoonbill, new Fish("fish")};
+
+        for (Animal animal : animalArray)
+            System.out.println(animal.toString());
+    }
 }
